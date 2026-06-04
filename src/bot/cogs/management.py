7ee733074037b0c51c51bfd016b933685aa24065
Anonymous <104823989+toast-riot@users.noreply.github.com>
2026-06-04 22:23:08 +1000

    impr: general app command error handling

--- src/bot/cogs/management.py
+++ src/bot/cogs/management.py
@@ -1,4 +1,4 @@
-from typing import final
+from typing import final, override
 
 import discord
 from discord import app_commands
@@ -9,9 +9,19 @@ from ..helpers import interactions
 
 @final
 class management(commands.Cog):
-    def __init__(self, bot: commands.Bot) -> None:
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.original_error_handler = bot.tree.on_error
+        self.bot.tree.error(self.on_app_command_error)
 
-    @app_commands.command(name="test")
-    async def test(self, interaction: discord.Interaction) -> None:
-        await interactions.respond(interaction, "Test command", ephemeral=True)
+    @override
+    async def cog_unload(self):
+        self.bot.tree.error(self.original_error_handler)
+
+    async def on_app_command_error(self, interaction: discord.Interaction[commands.Bot], error: discord.app_commands.AppCommandError):
+        await interactions.error(interaction, "An error occurred.")
+        await self.original_error_handler(interaction, error)
+
+    @app_commands.command() # TODO: rm
+    async def test(self, interaction: discord.Interaction[commands.Bot]):
+        await interactions.respond(interaction, "Test message")
