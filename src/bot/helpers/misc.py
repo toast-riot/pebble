949644046f95763b771a000c1a87dc6b5d38e5dc
Anonymous <104823989+toast-riot@users.noreply.github.com>
2026-06-09 21:56:15 +1000

    refactor: extract missing permission logic

--- src/bot/helpers/misc.py
+++ src/bot/helpers/misc.py
@@ -4,15 +4,21 @@ from discord.permissions import Permissions
 from .exceptions import PermissionException
 
 
+def missing_perms(target: Permissions, current: Permissions) -> list[str]:
+    return [k for k, v in (target & ~current) if v]
+
+
 def check_perms(channel: GuildChannel, perms: Permissions) -> None:
     current = channel.permissions_for(channel.guild.me)
-    missing_perms = [k for k, v in (perms & ~current) if v]
+    missing = missing_perms(perms, current)
 
     # if the bot can't view the channel, the missing permissions list is generally incorrect
-    if "read_messages" in missing_perms:
-        raise PermissionException(f"Missing permission to view channel {channel.mention}")
+    if "read_messages" in missing:
+        raise PermissionException(
+            f"Missing permission to view channel {channel.mention}"
+        )
 
-    if missing_perms:
-        lst = "\n".join("\\- " + perm for perm in missing_perms)
+    if missing:
+        lst = "\n".join("\\- " + perm for perm in missing)
         msg = f"Missing permissions for {channel.mention}:\n{lst}"
         raise PermissionException(msg)
