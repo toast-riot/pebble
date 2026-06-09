bcd59df94ab48bc1020cf0b11c3e8806c6f9fd95
Anonymous <104823989+toast-riot@users.noreply.github.com>
2026-06-09 20:47:43 +1000

    fix: update pins cog

--- src/bot/cogs/pins.py
+++ src/bot/cogs/pins.py
@@ -1,13 +1,14 @@
 from typing import final, override
-from urllib.parse import urlparse
 
 import discord
-from discord import app_commands
+from discord import MessageReferenceType, app_commands
 from discord.ext import commands
+from discord.permissions import Permissions
 
 from ..config import CFG
 from ..helpers import interactions
 from ..helpers.exceptions import BotException, ConfigurationException
+from ..helpers.misc import check_perms
 
 
 @final
@@ -37,86 +38,48 @@ class pins(commands.Cog):
         await interaction.response.defer()
         await self.pinboard(interaction, message)
 
-
-    async def get_embed(self, message: discord.Message) -> discord.Embed:
-        embed = discord.Embed(
-            description = message.content,
-            color = 0x2b2d31,
-            url=message.jump_url # Used to quickly find duplicate pins
-        )
-        embed.add_field(name="", value="", inline=False) # Spacer
-
-        if message.embeds:
-            data = message.embeds[0]
-            if data.type == "image":
-                embed.set_image(url=data.url)
-
-        if message.attachments:
-            attachment = message.attachments[0]
-            path = urlparse(attachment.url).path
-            if path.lower().endswith(("png", "jpeg", "jpg", "gif", "webp")):
-                embed.set_image(url=attachment.url)
-            else:
-                embed.add_field(name="Attachment", value=f"-# {attachment.url}", inline=False)
-
-        embed.add_field(name="User", value=f"-# {message.author.mention}")
-        embed.add_field(name="Link", value=f"-# {message.jump_url}")
-        embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
-
-        return embed
-
-
     async def pinboard(
         self, interaction: discord.Interaction, message: discord.Message
     ) -> None:
-        # await message.forward(interaction.channel)
-        # await interactions.respond(interaction, content=f"Message pinned to {interaction.channel.mention}")
+        assert interaction.guild
+        assert isinstance(message.channel, discord.abc.GuildChannel)
 
-        await message.channel.send()
+        server_config = CFG.get_server(interaction.guild.id)
 
-        await interactions.respond(
-            interaction, content=f"Message pinned to {message.channel.mention}"
-        )
+        is_nsfw = message.channel.is_nsfw() or message.channel.id in server_config.nsfw_extras
+        pin_channel_id = server_config.channel_pins_nsfw if is_nsfw else server_config.channel_pins
 
-        is_nsfw = message.channel.is_nsfw() or message.channel.id in config.server(interaction.guild.id).nsfw_extras
+        if not pin_channel_id:
+            if is_nsfw:
+                raise ConfigurationException("NSFW pinboard channel not configured")
+            raise ConfigurationException("Pinboard channel not configured")
 
-        if is_nsfw:
-            channel_to_get = config.server(interaction.guild.id).channel_pins_nsfw
-        else:
-            channel_to_get = config.server(interaction.guild.id).channel_pins
+        pin_channel = interaction.guild.get_channel(pin_channel_id)
 
-        pin_channel = interaction.guild.get_channel(channel_to_get)
+        if not pin_channel:
+            raise BotException(f"Pinboard channel `{pin_channel_id}` not found")
+        
+        if not isinstance(pin_channel, discord.TextChannel):
+            raise BotException(f"Pinboard channel {pin_channel.mention} is not a text channel")
 
-        # if not pin_channel:
-            # raise BotException(f"Pinboard channel not found (looking for `#{channel_to_get}`)")
-            # await interaction.edit_original_response(content=f"Pinboard channel not found (looking for `#{channel_to_get}`)")
-            # return
+        perms = Permissions(view_channel=True, send_messages=True)
+        if server_config.duplicate_pins_check_count > 0:
+            perms.read_message_history = True
+        check_perms(pin_channel, perms)
 
-        # if not pin_channel.permissions_for(message.guild.me).view_channel:
-        #     await perm_error(interaction, f"view {pin_channel.mention}")
-        #     return
-        # if not pin_channel.permissions_for(message.guild.me).send_messages:
-        #     await perm_error(interaction, f"send messages in {pin_channel.mention}")
-        #     return
-        # if not pin_channel.permissions_for(message.guild.me).embed_links:
-        #     await perm_error(interaction, f"embed links in {pin_channel.mention}")
-        #     return
-        # if CONFIG["nsfw_pin_channel_check_enabled"] and (should_be_nsfw and not pin_channel.is_nsfw()):
-        #     await interaction.edit_original_response(content=f"{pin_channel.mention} must be marked as NSFW")
-        #     return
+        if server_config.duplicate_pins_check_count > 0:
+            async for pin_message in pin_channel.history(limit=server_config.duplicate_pins_check_count):
+                if (
+                    pin_message.reference and
+                    pin_message.reference.message_id == message.id and
+                    pin_message.reference.type == MessageReferenceType.forward
+                ):
+                    raise BotException(f"Message is already pinned at {pin_message.jump_url}")
 
-        # Check if message is already pinned
-        if config.server(interaction.guild.id).duplicate_pins_check_count > 0:
-            # if not pin_channel.permissions_for(message.guild.me).read_message_history:
-            #     await perm_error(interaction, f"read message history in {pin_channel.mention}")
-            #     return
+        try:
+            await pin_channel.send(f"-# {message.author.mention}", allowed_mentions=discord.AllowedMentions.none())
+            pin = await message.forward(pin_channel)
+        except discord.HTTPException as e:
+            raise BotException(e.text, handled=False) from e
 
-            async for pin_message in pin_channel.history(limit=config.server(interaction.guild.id).duplicate_pins_check_count):
-                current = pin_message.embeds and pin_message.embeds[0] and pin_message.embeds[0].url or None
-                if current == message.jump_url:
-                    await interactions.respond(interaction, content=f"Message is already pinned at {pin_message.jump_url}")
-                    return
-
-        embed = await self.get_embed(message)
-        await pin_channel.send(embed=embed)
-        await interactions.respond(interaction, content=f"Message pinned to {pin_channel.mention}")
+        await interactions.respond(interaction, content=f"Message pinned: {pin.jump_url}")
